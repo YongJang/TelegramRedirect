2bbec4b72f8856e33cea9339dc1ffcfdfcbd7c04
YongJang <yj_430@naver.com>
2016-05-21 21:57:20 +0900

    aaa

--- index.php
+++ index.php
@@ -1,9 +1,31 @@
-﻿<html>
+<?php
+    $servername = "telegramdb.cctjzlx6kmlc.ap-northeast-1.rds.amazonaws.com";
+    $username = "yongjang";
+    $password = "yongjang";
+    $mysql_db = "telegramdb";
+
+    $conn = new mysql_connect($servername, $username, $password);
+    $dbconn = mysql_select_db($mysql_db, $conn);
+
+    mysql_query("set names utf8");
+
+    $url = $_GET["url"];
+
+    $sql = "SELECT click_num, meta FROM jobs WHERE url = ".$url;
+
+    $result = mysql_query($sql, $conn);
+
+    $click_num;
+    $meta;
+    while($row = mysql_fetch_array($result)){
+      $click_num = $row['click_num'];
+      $meta = $row['meta'];
+    }
+ ?>
+<html>
         <head>
           <?php
-          $hh = "<h1>This is Header!!</h1>";
-
-          echo $hh;
+          echo $meta;
           ?>
 
 
@@ -11,9 +33,7 @@
         </head>
         <body>
               <?php
-              $welcome = "wellcome!!";
-              echo $welcome;
+                echo $meta;
               ?>
-                <meta content="메디아나 소프트웨어/Embedded System 개발자 - 잡코리아" name="title"/><meta content="메디아나 소프트웨어/Embedded System 개발자 경력 : 신입·경력2년이상, 학력 : 초대졸이상, 급여 : 회사내규에 따름, 고용형태 : 정규직, 성별 : 성별무관, 나이 : , 마감일 : 2016.06.30, 홈페이지 : http://www.mediana.co.kr" name="description"/>
         </body>
 </html>
