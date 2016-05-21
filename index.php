a1ae37b63a4548b7d6179598de02d33a7a29bd94
YongJang <yj_430@naver.com>
2016-05-21 21:24:18 +0900

    aaa

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 ﻿<html>
         <head>
           <?php
+          $hh = "<h1>This is Header!!</h1>";
 
-
-            
+          echo $hh;
           ?>
 
 
@@ -11,7 +11,7 @@
         </head>
         <body>
               <?php
-              $welcome = "wellcome!!"
+              $welcome = "wellcome!!";
               echo $welcome;
               ?>
                 <meta content="메디아나 소프트웨어/Embedded System 개발자 - 잡코리아" name="title"/><meta content="메디아나 소프트웨어/Embedded System 개발자 경력 : 신입·경력2년이상, 학력 : 초대졸이상, 급여 : 회사내규에 따름, 고용형태 : 정규직, 성별 : 성별무관, 나이 : , 마감일 : 2016.06.30, 홈페이지 : http://www.mediana.co.kr" name="description"/>
